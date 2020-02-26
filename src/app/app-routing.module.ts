5b1a37a253e4504cc6d41f6ee4a67f55282ba60a
Bowragon <ragnarokatz@gmail.com>
2020-02-26 16:41:38 -0500

    Remove original template components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./home.component";
-import { AboutComponent } from './about.component';
-import { ContactComponent } from "./contact.component";
+import { QuestionListComponent } from './question-list.component';
+import { QuestionDetailComponent } from "./question-detail.component";
 import { NotFoundComponent } from "./not-found.component";
-import { VehicleListComponent } from './vehicle-list.component';
-import { VehicleDetailComponent } from './vehicle-detail.component';
-import { VehicleEditComponent } from './vehicle-edit.component';
-import { VehicleDeleteComponent } from './vehicle-delete.component';
-import { VehicleCreateComponent } from './vehicle-create.component';
 // import more components here
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'vehicles', component: VehicleListComponent },
-  { path: 'vehicles/create', component: VehicleCreateComponent },
-  { path: 'vehicles/detail/:id', component: VehicleDetailComponent },
-  { path: 'vehicles/edit/:id', component: VehicleEditComponent },
-  { path: 'vehicles/delete/:id', component: VehicleDeleteComponent },
+  { path: 'questions', component: QuestionListComponent },
+  { path: 'questions/detail:id', component: QuestionDetailComponent },
   // more routes are added here
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
--- src/app/data-classes.ts
+++ src/app/data-classes.ts
@@ -1,22 +1,6 @@
 // Data model classes for the app
 
-// Describes a product
-export class Product {
-  id: number;
-  name: string;
-}
-
-// Describes a vehicle
-export class Vehicle {
-  id: number;
-  make: string;
-  model: string;
-  year: number;
-  msrp: number;
-  photo: string;
-  description: string;
-}
-
+// Describes a question
 export class Question {
   order: number;
   question: string;
