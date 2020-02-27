3e58ba8334bd3090feaaf85d3bfd05c8989ea2d3
Bowragon <ragnarokatz@gmail.com>
2020-02-27 14:48:44 -0500

    Add simple express server to host built angular app on web, following instructions online

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-tutorial!');
+    expect(page.getTitleText()).toEqual('安省G1考试通');
   });
 
   afterEach(async () => {
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/angular-tutorial'),
+      dir: require('path').join(__dirname, '../coverage/g1-exam-webapp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
